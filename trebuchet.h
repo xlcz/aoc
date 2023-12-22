6a4ddd6dda49b92b4240777e9aae837f9a31fa2d
max <109116116+xlcz@users.noreply.github.com>
2023-12-22 02:27:40 +0000

    improved code and clarifications
    help from together c&cpp discord server

--- trebuchet.h
+++ trebuchet.h
@@ -15,17 +15,14 @@ void trebuchet() {
     while (fgets(line, sizeof(line), file)) {
         int firstDigit = -1, lastDigit = 0;
 
-        for (int i = 0; i < sizeof(line); i++) { // Foreach line in fgets
+        for (int i = 0; i < sizeof(line) / sizeof(line[0]); i++) { // Foreach line in fgets
             if (line[i] == '\n') break; // End of sentence, skip current iterator
-            const int asInt = (line[i] - 0) - 48; // Convert to integer values (0-9)
+            const int asInt = line[i] - '0'; // Convert to integer values (0-9)
             if (asInt < 0 || asInt > 9) continue; // not an integer
-            if (firstDigit == -1) {
-                firstDigit = asInt * 10; // Emulates string concatenation, first digit will always be * 10 while second digit is added
-            }
+            if (firstDigit == -1) firstDigit = asInt * 10; // Emulates string concatenation, first digit will always be * 10 while second digit is added
             lastDigit = asInt;
         }
         counter += (firstDigit + lastDigit); // Increment counter
-        line[0] = "\0"; // clear line
     }
     printf("%i", counter); // Print output
     fclose(file);
